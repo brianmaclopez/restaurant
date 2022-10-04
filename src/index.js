aa38703911c5725d3b06825df991c3e5e5d016c0
Lugh_Lámfada <healingphases@gmail.com>
2022-10-04 15:03:56 -0700

    devserver functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './styles/style.css';
+
 console.log("So far, so good");
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   devServer: {
     static: './dist',
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  optimization: {
-    runtimeChunk: 'single',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
+  mode: "development",
 };
